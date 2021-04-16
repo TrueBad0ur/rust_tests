3937c75e2cc1a02c6e1ad04a1d74975dabc81e1c
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-04-16 11:12:10 +0300

    Lifetime test

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
-    let a = longest("a", "Hel");
-        println!("For tests1");
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
