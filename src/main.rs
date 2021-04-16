5cc04b3ca1510580c5d4db63a528913e24fadad4
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-04-16 11:25:53 +0300

    Local lifetime is different

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn longest<'a>(x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
 }
 
 fn main() {
     let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
+
+    println!("The longest string is {}", string1);
 }
 
