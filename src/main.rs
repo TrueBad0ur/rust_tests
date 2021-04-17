b563c29a5ce1e14baa65ca0dc7f0120d18be6690
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-04-17 12:05:22 +0300

    First commit!

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use std::fmt::Display;
-
-fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T, ) -> &'a str where T: Display, {
-    println!("Announcement! {}", ann);
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
-}
-
-fn main() {
-    let x = "zalatustra";
-    print!("{}", longest_with_an_announcement("abcd", "abcde", "haha"));
+fn main()  {
+    print!("Hello, world!");
 }
